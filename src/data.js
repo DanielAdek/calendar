7e745909e838ab0bdc4567830cc78abd96c51254
DanielAdek <maildaniel.me1@gmail.com>
2019-05-27 15:12:51 +0100

    feat: add calendar

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        date: 1,
+        month: 'may',
+        space: '10',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 2,
+        month: 'may',
+        space: '19',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 3,
+        month: 'may',
+        space: '9',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 4,
+        month: 'may',
+        space: '17',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 5,
+        month: 'may',
+        space: '15',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 6,
+        month: 'may',
+        space: '13',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 7,
+        month: 'may',
+        space: '10',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 8,
+        month: 'may',
+        space: '5',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 9,
+        month: 'may',
+        space: '18',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 10,
+        month: 'may',
+        space: '14',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 11,
+        month: 'may',
+        space: '18',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 12,
+        month: 'may',
+        space: '16',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    },
+    {
+        date: 13,
+        month: 'may',
+        space: '12',
+        timeSlot: ['09:00 AM', '09:00 PM']
+    }
+]
